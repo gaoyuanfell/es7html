cbabf5b3959c439e78a3b43fc8f8cc01d007ec42
cuiyongli <cuiyongli@ycmedia.com>
2017-12-08 17:57:29 +0800

    1

--- app/lcinc-pc/lcincpc-index.js
+++ app/lcinc-pc/lcincpc-index.js
@@ -5,94 +5,85 @@ import '../../common/js/vendor'
 
 const filtersRef = document.querySelector("#filters");
 filtersRef.addEventListener('click', (e) => {
-    console.info(e)
-    let fRef = null;
     Array.from(filtersRef.children).forEach(f => {
         f.classList.remove('active')
         if (f.contains(e.target)) {
             f.classList.add('active')
-            fRef = f
         }
     })
-
-
 });
 
+//-----------------------------------------------------
+const oBtnLeft = document.getElementById("prev");
+const oBtnRight = document.getElementById("next");
+const oDiv = document.getElementById("indexmaindiv");
+const oUl = document.getElementById("product_list");
+const aLi = oUl.getElementsByClassName("list");
+const now = -4 * (aLi[0].offsetWidth);
+oUl.style.width = aLi.length * (aLi[0].offsetWidth) + 'px';
+oBtnRight.onclick = function () {
+    const n = Math.floor((aLi.length * (aLi[0].offsetWidth) + oUl.offsetLeft) / aLi[0].offsetWidth);
+
+    if (n <= 4) {
+        move(oUl, 'left', 0);
+    }
+    else {
+        move(oUl, 'left', oUl.offsetLeft + now);
+    }
+}
+oBtnLeft.onclick = function () {
+    const now1 = -Math.floor((aLi.length / 4)) * 4 * (aLi[0].offsetWidth);
+
+    if (oUl.offsetLeft >= 0) {
+        move(oUl, 'left', now1);
+    }
+    else {
+        move(oUl, 'left', oUl.offsetLeft - now);
+    }
+}
+let timer = setInterval(oBtnRight.onclick, 5000);
+oDiv.onmouseover = function () {
+    clearInterval(timer);
+}
+oDiv.onmouseout = function () {
+    timer = setInterval(oBtnRight.onclick, 5000);
+}
 
 
-window.onload = function () {
+function getStyle(obj, name) {
+    if (obj.currentStyle) {
+        return obj.currentStyle[name];
+    }
+    else {
+        return getComputedStyle(obj, false)[name];
+    }
+}
 
-    const oBtnLeft = document.getElementById("prev");
-    const oBtnRight = document.getElementById("next");
-    const oDiv = document.getElementById("indexmaindiv");
-    const oUl = document.getElementById("product_list");
-    const aLi = oUl.getElementsByClassName("list");
-    const now = -4 * (aLi[0].offsetWidth);
-    oUl.style.width = aLi.length * (aLi[0].offsetWidth) + 'px';
-    oBtnRight.onclick = function () {
-        const n = Math.floor((aLi.length * (aLi[0].offsetWidth) + oUl.offsetLeft) / aLi[0].offsetWidth);
-
-        if (n <= 4) {
-            move(oUl, 'left', 0);
+function move(obj, attr, iTarget) {
+    clearInterval(obj.timer)
+    obj.timer = setInterval(function () {
+        let cur = 0;
+        if (attr == 'opacity') {
+            cur = Math.round(parseFloat(getStyle(obj, attr)) * 100);
         }
         else {
-            move(oUl, 'left', oUl.offsetLeft + now);
+            cur = parseInt(getStyle(obj, attr));
         }
-    }
-    oBtnLeft.onclick = function () {
-        const now1 = -Math.floor((aLi.length / 4)) * 4 * (aLi[0].offsetWidth);
-
-        if (oUl.offsetLeft >= 0) {
-            move(oUl, 'left', now1);
+        let speed = (iTarget - cur) / 4;
+        speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+        if (iTarget == cur) {
+            clearInterval(obj.timer);
         }
-        else {
-            move(oUl, 'left', oUl.offsetLeft - now);
-        }
-    }
-    var timer = setInterval(oBtnRight.onclick, 5000);
-    oDiv.onmouseover = function () {
-        clearInterval(timer);
-    }
-    oDiv.onmouseout = function () {
-        timer = setInterval(oBtnRight.onclick, 5000);
-    }
-
-    };
-
-    function getStyle(obj, name) {
-        if (obj.currentStyle) {
-            return obj.currentStyle[name];
+        else if (attr == 'opacity') {
+            obj.style.filter = 'alpha(opacity:' + (cur + speed) + ')';
+            obj.style.opacity = (cur + speed) / 100;
         }
         else {
-            return getComputedStyle(obj, false)[name];
+            obj.style[attr] = cur + speed + 'px';
         }
-    }
+    }, 30);
+}
 
-    function move(obj, attr, iTarget) {
-        clearInterval(obj.timer)
-        obj.timer = setInterval(function () {
-            var cur = 0;
-            if (attr == 'opacity') {
-                cur = Math.round(parseFloat(getStyle(obj, attr)) * 100);
-            }
-            else {
-                cur = parseInt(getStyle(obj, attr));
-            }
-            var speed = (iTarget - cur) / 4;
-            speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-            if (iTarget == cur) {
-                clearInterval(obj.timer);
-            }
-            else if (attr == 'opacity') {
-                obj.style.filter = 'alpha(opacity:' + (cur + speed) + ')';
-                obj.style.opacity = (cur + speed) / 100;
-            }
-            else {
-                obj.style[attr] = cur + speed + 'px';
-            }
-        }, 30);
-    }
-
-
+//-----------------------------------------------------
 
 
