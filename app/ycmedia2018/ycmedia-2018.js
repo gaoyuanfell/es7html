4033edd564e3c1b17df3e345349dc504204123bb
cuiyongli <cuiyongli@ycmedia.com>
2018-05-16 13:40:32 +0800

    1

--- app/ycmedia2018/ycmedia-2018.js
+++ app/ycmedia2018/ycmedia-2018.js
@@ -1246,3 +1246,95 @@ function removeStore(name) {
 
 document.querySelector('#nowyear').innerHTML = new Date().getFullYear()
 
+/*隐私条例*/
+
+$('#about').on('click', function () {
+    if($('#yccm_client').hasClass("yccm_client2")){
+
+    }else{
+        $('#yccm_client').addClass("yccm_client2");
+        layer.open({
+            type: 1,
+            title: false,
+            shadeClose: true,
+            shade: 0,
+            end:function(){
+                $('#yccm_client').removeClass("yccm_client2");
+            },
+            area: ['100%', '100%'],
+            content:$('#yccm_client')
+        });
+    }
+});
+$('#click_here').on('click', function(){
+    $.get("http://stat.adpush.cn/ClearCookie.ashx" ,{})
+    $('#out').show();
+    $('.yccm_body').hide();
+    let ch=document.documentElement.clientHeight||document.body.clientHeight;
+    $('#out').css('height',ch-289);
+    //窗口改动的时候
+    window.onresize=function(){
+        //获取窗口高度
+        let ch=document.documentElement.clientHeight||document.body.clientHeight;
+        $('#out').css('height',ch-289);
+    }
+})
+
+
+
+var wrap=document.getElementById('wrap'),
+    pic=document.getElementById('pic'),
+    list=document.getElementById('list').getElementsByTagName('li'),
+    index=0,
+    timer=null;
+
+// 定义并调用自动播放函数
+if(timer){
+
+    clearInterval(timer);
+    timer=null;
+}
+timer=setInterval(autoplay,2000);
+// 定义图片切换函数
+function autoplay(){
+    index++;
+    if(index>=list.length){
+        index=0;
+    }
+    changeoptions(index);
+
+
+}
+
+// 鼠标划过整个容器时停止自动播放
+wrap.onmouseover=function(){
+
+    clearInterval(timer);
+
+}
+// 鼠标离开整个容器时继续播放至下一张
+wrap.onmouseout=function(){
+
+    timer=setInterval(autoplay,2000);
+}
+// 遍历所有数字导航实现划过切换至对应的图片
+for(var i=0;i<list.length;i++){
+    list[i].id=i;
+    list[i].onmouseover=function(){
+        clearInterval(timer);
+        changeoptions(this.id);
+
+    }
+}
+function changeoptions(curindex){
+    for(var j=0;j<list.length;j++){
+        list[j].className='';
+        pic.style.top=0;
+
+    }
+    list[curindex].className='active';
+    pic.style.top=-curindex*240+'px';
+    index=curindex;
+}
+
+
