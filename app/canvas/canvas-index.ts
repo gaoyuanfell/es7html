4a8c427c0bd659be64c7190449021226862a78e3
moka <gaoyuanfell@sina.com>
2018-07-12 10:53:11 +0800

    1

--- app/canvas/canvas-index.ts
+++ app/canvas/canvas-index.ts
@@ -1,124 +1,18 @@
-let canvas = <HTMLCanvasElement>document.getElementById('tutorial')
-let ctx = canvas.getContext('2d')
+let canvas = <HTMLCanvasElement>document.getElementById('tutorial');
+let canvas2 = <HTMLCanvasElement>document.getElementById('tutorial2');
+let ctx = canvas.getContext('2d');
+let ctx2 = canvas2.getContext('2d');
 
-function demo1() {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(50, 50, 200, 200);
+let titleRef = <HTMLInputElement>document.getElementById('title');
+let describeRef = <HTMLInputElement>document.getElementById('describe');
 
-    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    ctx.fillRect(200, 200, 100, 100)
-}
+titleRef.addEventListener('input', draw2);
+describeRef.addEventListener('input', draw2);
 
-// demo1()
+document.getElementById('result').addEventListener('click', result)
 
-function demo2() {
-    ctx.fillRect(50, 50, 200, 200);
-    ctx.clearRect(80, 80, 80, 80)
-    ctx.strokeRect(90, 90, 40, 40)
-}
 
-// demo2()
-
-function demo3() {
-    ctx.beginPath();
-    ctx.moveTo(75, 50);
-    ctx.lineTo(100, 75);
-    ctx.lineTo(100, 25);
-    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    ctx.fill();
-}
-
-// demo3();
-
-function demo4() {
-    ctx.beginPath()
-    ctx.arc(175, 175, 50, 0, Math.PI * 2, true); // 绘制
-    ctx.moveTo(210, 175);
-    ctx.arc(175, 175, 35, 0, Math.PI, false);   // 口(顺时针)
-    ctx.moveTo(165, 165);
-    ctx.arc(160, 165, 5, 0, Math.PI * 2, true);  // 左眼
-    ctx.moveTo(195, 165);
-    ctx.arc(190, 165, 5, 0, Math.PI * 2, true);  // 右眼
-    ctx.stroke()
-    ctx.closePath()
-}
-
-// demo4()
-
-function demo5() {
-    ctx.beginPath();
-    ctx.moveTo(25, 25);
-    ctx.lineTo(105, 25);
-    ctx.lineTo(25, 105);
-    ctx.fill();
-}
-
-// demo5()
-
-function demo6() {
-    ctx.beginPath();
-    ctx.moveTo(125, 125);
-    ctx.lineTo(125, 45);
-    ctx.lineTo(45, 125);
-    ctx.closePath();
-    ctx.stroke();
-}
-
-// demo6()
-
-function demo7() {
-    ctx.beginPath();
-    ctx.moveTo(75, 0);
-    ctx.quadraticCurveTo(0, 0, 0, 75);
-    ctx.quadraticCurveTo(0, 150, 75, 150);
-    // ctx.moveTo(75,150);
-    ctx.quadraticCurveTo(150, 150, 150, 75);
-    ctx.quadraticCurveTo(150, 0, 75, 0);
-    ctx.strokeStyle = '#000'
-    ctx.stroke();
-    ctx.fillStyle = 'red'
-    ctx.fill()
-}
-
-// demo7();
-
-function demo8() {
-    for (let i = 0; i < 6; i++) {
-        for (let j = 0; j < 6; j++) {
-            ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ',' + Math.floor(255 - 42.5 * j) + ',0)';
-            ctx.fillRect(j * 25, i * 25, 25, 25);
-        }
-    }
-}
-
-// demo8()
-
-function fillText() {
-    ctx.font = "48px serif";
-    ctx.fillText("Hello world", 500, 50);
-    ctx.font = "48px serif";
-    ctx.strokeText("Hello world", 10, 50);
-}
-
-// fillText()
-
-// fontSize fontFamily color text x y
-function text(options) {
-    ctx.font = `${options.fontSize || '12'}px ${options.fontFamily || 'serif'}`;
-    ctx.fillText(options.text, options.x, options.y);
-}
-
-document.getElementById('title').addEventListener('input', (event: any) => {
-    text({
-        fontSize: 24,
-        fontFamily: 'serif',
-        x: 10,
-        y: 24,
-        text: event.target.value
-    })
-})
-
-function demo9() {
+function draw() {
     let img = new Image();
     img.onload = () => {
         let width = img.width;
@@ -127,81 +21,107 @@ function demo9() {
         let w = ctx.canvas.width;
         let h = ctx.canvas.height;
 
-        console.info(ctx.canvas.width);
+        if (width > height) {
 
-        ctx.drawImage(img, 0, 0, img.width / 8, img.height / 8);
-        console.info(img)
+        } else {
+            let sx = 0;
+            let sy = 0;
+            let sWidth = width;
+            let sHeight = height;
+            let dx = 0;
+            let dy = 0;
+            let dWidth = w;
+            let dHeight = w / width * height;
+            ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+        }
     }
     img.src = 'static/img/1.jpg'
 }
 
-// demo9();
-// let raf;
-// let ball = {
-//     x: 100,
-//     y: 100,
-//     vx: 5,
-//     vy: 2,
-//     radius: 25,
-//     color: 'blue',
-//     draw: function() {
-//         ctx.beginPath();
-//         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-//         ctx.closePath();
-//         ctx.fillStyle = this.color;
-//         ctx.fill();
-//     }
-// };
-//
-// function draw() {
-//     ctx.clearRect(0,0, canvas.width, canvas.height);
-//     ball.draw();
-//     ball.vy *= .99;
-//     ball.vy += .25;
-//     ball.x += ball.vx;
-//     ball.y += ball.vy;
-//     if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
-//         ball.vy = -ball.vy;
-//     }
-//     if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
-//         ball.vx = -ball.vx;
-//     }
-//     raf = window.requestAnimationFrame(draw);
-// }
-//
-//
-// canvas.addEventListener('mouseover', function(e){
-//     raf = window.requestAnimationFrame(draw);
-// });
-//
-// canvas.addEventListener('mouseout', function(e){
-//     window.cancelAnimationFrame(raf);
-// });
-//
-// ball.draw();
+draw();
 
-function demo10() {
-    let img = new Image();
-    img.onload = () => {
-        ctx.drawImage(img, 0, 0);
-        // img.style.display = 'none';
-    }
-    img.src = 'static/img/1.jpg'
 
-    var color = document.getElementById('color');
-    function pick(event) {
-        var x = event.layerX;
-        var y = event.layerY;
-        var pixel = ctx.getImageData(x, y, 1, 1);
-        console.info(pixel)
-        var data = pixel.data;
-        var rgba = 'rgba(' + data[0] + ',' + data[1] +
-            ',' + data[2] + ',' + (data[3] / 255) + ')';
-        color.style.background =  rgba;
-        color.textContent = rgba;
-    }
-    canvas.addEventListener('mousemove', pick);
-
+function draw2() {
+    ctx2.clearRect(0, 0, ctx2.canvas.width, ctx2.canvas.height);
+    ctx2.font = "48px serif";
+    ctx2.fillText(titleRef.value, 0, 48);
+    ctx2.fillText(describeRef.value, 0, 96)
 }
 
-demo10();
+function canvasToBlob(canvas) {
+    return new Promise((resolve, reject) => {
+        canvas.toBlob(function (blob) {
+            resolve(blob)
+        })
+    })
+}
+
+function result() {
+    let list = [canvas, canvas2]
+
+    let _canvas = <HTMLCanvasElement>document.createElement('canvas')
+    _canvas.width = 500;
+    _canvas.height = 500;
+    _canvas.style.display = 'none';
+    document.body.appendChild(_canvas)
+    let _ctx = _canvas.getContext('2d')
+    list.every(li => {
+        _ctx.drawImage(li, 0, 0)
+        return true;
+    })
+
+    canvasToBlob(_canvas).then(blob => {
+        let url = URL.createObjectURL(blob);
+        let image = <HTMLImageElement>document.createElement('img');
+        image.src = url;
+        document.body.appendChild(image);
+    })
+}
+
+let elementList = [
+    {
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+        color: '#000',
+        type: 'text',
+        fontSize: 24,
+        fontFamily: 'serif',
+        value: 'XXX让你的旅行如此轻松',
+        show: true,
+    },
+    {
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+        color: '#000',
+        type: 'text',
+        fontSize: 24,
+        fontFamily: 'serif',
+        value: '出境游,很难吗？',
+        show: true,
+    },
+    {
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+        color: '#000',
+        type: 'text',
+        fontSize: 24,
+        fontFamily: 'serif',
+        value: 'SUMMER',
+        show: true,
+    }
+]
+
+let canvasBox = {
+    width: 900,
+    height: 400,
+}
+
+function init() {
+
+}
